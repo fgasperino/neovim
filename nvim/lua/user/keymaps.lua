880063825a3383a0c8478126f600ff3516e8a9b6
fgasperino <franco.gasperino@gmail.com>
2024-07-01 08:25:31 -0700

    * Update keymaps for neovim-tree.

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -50,8 +50,8 @@ keymap.set('n', '<Down>', ':resize -1<cr>', { desc = 'Resize pane (horiz, shrink
 --
 
 -- Nvim-Tree
-keymap.set('n', '<leader>tt', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle file tree.' })
-keymap.set('n', '<leader>ta', '<cmd>NvimTreeFindFileToggle<cr>', { desc = 'Toggle file tree at current buffer locataion.'})
+keymap.set('n', '<leader>ntt', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle file tree.' })
+keymap.set('n', '<leader>nta', '<cmd>NvimTreeFindFileToggle<cr>', { desc = 'Toggle file tree at current buffer locataion.'})
 
 -- Telescope
 local builtin = require('telescope.builtin')
