774b59c6c7bbfc13d60f2b04d5c626ebe2c3bcaa
fgasperino <franco.gasperino@gmail.com>
2024-09-26 10:25:54 -0700

    * Added theme override behavior

---
+++ nvim/after/plugin/theme.lua
@@ -0,0 +1,7 @@
+-- 
+-- Settings for post-theme plugin overrides.
+-- 
+
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='grey'})
+vim.api.nvim_set_hl(0, 'LineNr', { fg='white'})
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='grey'})
