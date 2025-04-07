7130cf4eb6b7a267d5b9716cd720e09cdfc73638
fgasperino <franco.gasperino@gmail.com>
2025-04-07 12:34:20 -0700

    * Updated config for Neovim 0.11.0.

--- nvim/lua/user/globals.lua
+++ nvim/lua/user/globals.lua
@@ -7,3 +7,10 @@ local g = vim.g
 -- Disable explorer
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
+
+-- Diagnostics -- 
+
+local d = vim.diagnostic
+
+-- Enable virtual text 
+d.config({ virtual_text = true})
